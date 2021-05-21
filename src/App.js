be11fa8afa9288b3a38a06aa1d61516d7568775a
Mariam  Khamo <mariamw736@gmail.com>
2021-05-21 09:12:03 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import {CatComponent} from './CatComponent';
-import {GraceHopperQuoteComponent} from './GraceHopperQuoteComponent.js';
-import {MouseComponent} from './MouseComponent';
+import CatComponent from './CatComponent.js';
+import GraceHopperQuoteComponent from './GraceHopperQuoteComponent.js';
+import MouseComponent from './MouseComponent.js';
 
 
 
